542168fba6b8e45fc7f8f0609e50d9121d071096
MoraisSousa <guilherme.gms.93@hotmail.com>
2019-04-06 15:41:34 -0300

    Regex

--- ByteBank.SistemaAgencia/Program.cs
+++ ByteBank.SistemaAgencia/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace ByteBank.SistemaAgencia
 {
@@ -6,13 +7,23 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
-            string url = "www.bytebank.com/cambio?origem=real&destino=dolar&valor=1500";
+            //string padrao = "[0123456789][0123456789][0123456789][0123456789][-][0123456789][0123456789][0123456789][0123456789]";
+            string padrao = "[0-9]{4,5}-?[0-9]{4}";
 
-            ExtratorValorDeArgumentosURL extrator = new ExtratorValorDeArgumentosURL(url);
 
-            Console.WriteLine(extrator.GetValor("origem"));
-            Console.WriteLine(extrator.GetValor("destino"));
-            Console.WriteLine(extrator.GetValor("valor"));
+            string texto = "Meu número é: 923423453";
+
+            Match match = Regex.Match(texto, padrao);
+            Console.WriteLine(match.Value);
+
+
+            //string url = "www.bytebank.com/cambio?origem=real&destino=dolar&valor=1500";
+
+            //ExtratorValorDeArgumentosURL extrator = new ExtratorValorDeArgumentosURL(url);
+
+            //Console.WriteLine(extrator.GetValor("origem"));
+            //Console.WriteLine(extrator.GetValor("destino"));
+            //Console.WriteLine(extrator.GetValor("valor"));
 
 
             Console.ReadLine();

9c043112652c6b11ffd04b1c5443f155fea47137
MoraisSousa <guilherme.gms.93@hotmail.com>
2019-05-08 16:30:16 -0300

    Classe generica para CRUD LIST

--- ByteBank.SistemaAgencia/Program.cs
+++ ByteBank.SistemaAgencia/Program.cs
@@ -7,28 +7,39 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
+            Lista<int> idades = new Lista<int>();
 
+            idades.AdicionarVarios(1, 15, 51, 56);
+            idades.Remover(15);
+            idades.EscreverListaNaTela();
+            Lista<string> cursos = new Lista<string>();
+            cursos.AdicionarVarios("C# Parte 1", "C# Parte 2", "C# Parte 3");
+            cursos.EscreverListaNaTela();
 
-            ListaDeContaCorrente lista = new ListaDeContaCorrente();
+            Lista<ContaCorrente> contas = new Lista<ContaCorrente>();
+            contas.AdicionarVarios(new ContaCorrente(124, 54354), new ContaCorrente(201, 44354));
+            contas.EscreverListaNaTela();
 
-            ContaCorrente contaGui = new ContaCorrente(111, 11111);
+            //ListaDeContaCorrente lista = new ListaDeContaCorrente();
 
-            lista.AdicionarVarios(
-                new ContaCorrente(100, 40010),
-                new ContaCorrente(101, 40011),
-                new ContaCorrente(102, 40012),
-                new ContaCorrente(103, 40013)
-            );
+            //ContaCorrente contaGui = new ContaCorrente(111, 11111);
+
+            //lista.AdicionarVarios(
+            //    new ContaCorrente(100, 40010),
+            //    new ContaCorrente(101, 40011),
+            //    new ContaCorrente(102, 40012),
+            //    new ContaCorrente(103, 40013)
+            //);
             //lista.EscreverListaNaTela();
             //lista.Remover(contaGui);
             //Console.WriteLine("Item removido");
             //lista.EscreverListaNaTela();
 
-            for (int i = 0; i < lista.tamanho; i++)
-            {
-                ContaCorrente conta = lista[i];
-                Console.WriteLine($"{conta.Agencia}/{conta.Numero}");
-            }
+            //for (int i = 0; i < lista.tamanho; i++)
+            //{
+            //    ContaCorrente conta = lista[i];
+            //    Console.WriteLine($"{conta.Agencia}/{conta.Numero}");
+            //}
 
             Console.ReadLine();
         }
